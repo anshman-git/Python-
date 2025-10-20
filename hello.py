8061b29d0ba2b47aafbd255ed0431fccac364591
Anshman Khare <anshmankhare@gmail.com>
2025-10-20 23:50:51 +0530

    day 2

--- hello.py
+++ hello.py
@@ -1,1 +1,70 @@
-print("Hello World")
+# CHAPTER 1              MODULE,COMMENTS & PIP
+# print("Hello World")
+# print(''' Welcome to Anshman codes 
+# here are some python codes''')
+
+# CHAPTER 2              VARIABLES AND DATATYPES
+# print sum of two number
+# a=5
+# b=6
+# c=a+b
+# print(c)
+
+# print sum of two number given by user
+# a=int(input("Enter a:"))
+# b=int(input("Enter b:"))
+# c=a+b
+# print(c)
+
+# write a python program to find remainder when a no. divided by z
+# a=int(input("Enter any number:"))
+# z=int(input("Enter z:"))
+# b=a%z
+# print("remainder is ",b)
+
+# check the type of variable assighned using input function 
+# a=3
+# b='a'
+# print(type(a))
+# print(type(b))
+
+# use comparison to find out whether a given variable a is greater than b or not
+# a=int(input("Enter a:"))
+# b=int(input("Enter b:"))
+# print("a>b:",a>b)
+# print("a<b",a<b)
+
+# write a pyhon program to find average of two number entered by user 
+# a=int(input("Enter a:"))
+# b=int(input("Enter b:"))
+# print("Average of a and b is ",(a+b)/2)
+
+# write a program to calculate the sq. of a no. input by user
+# a=int(input("Enter no.:"))
+# print("Square of a is ",a*a)             #or print("Square of a is ",a**2)  iska matlab a ki power 2
+
+
+# CHAPTER 3             STRINGS
+
+# a= 'Single quote string'
+# b="Double quote string"
+# c='''Triple quote string'''
+# print(a)
+# print(b)
+# print(c)
+
+# String slicing
+# name = "ANSHMAN"
+# sl=name[2:4]
+# print(sl)
+# sl2=name[0:]
+# print(sl2)
+
+# string with skip values
+# word="abcdefgh"
+# print(word[1:4:2])       #it will print bd
+
+# String function
+a="anshman"
+# 1. len(a)  #return 4
+# 2. string.endwith("an") #returns true
