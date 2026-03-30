4d138661a9c2334db583b6747da24343bc7adaaf
Anshman Khare <anshmankhare@gmail.com>
2026-03-30 23:56:58 +0530

    45

--- daily.py
+++ daily.py
@@ -56,3 +56,11 @@
 #                 count += 1
 #             temp //= 10
 #         return count
+
+# 5.Write a program to print multiplication table of n using for loops in reversed order.
+# def multiplication_table(n):
+#                 for i in range(1, 11):
+#                         print(f"{n} x {11-i} = {n * (11-i)}")
+
+# n = int(input("Enter any number: "))
+# multiplication_table(n)
