8482d2094ec3688fc17251f334634f6e2c03f10f
Anshman Khare <anshmankhare@gmail.com>
2026-04-09 16:39:26 +0530

    6

--- daily.py
+++ daily.py
@@ -64,3 +64,17 @@
 
 # n = int(input("Enter any number: "))
 # multiplication_table(n)
+
+# 6.Move all zero to the end (g4g)
+# class Solution:
+# 	def pushZerosToEnd(self, arr):
+#     	count = 0 
+        
+#         for i in range(len(arr)):
+#             if arr[i] != 0:
+#                 arr[count] = arr[i]
+#                 count += 1
+        
+#         while count < len(arr):
+#             arr[count] = 0
+#             count += 1
