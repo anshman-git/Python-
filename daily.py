682bf372a928690891e1729d8c337ac6c8fed06f
Anshman Khare <anshmankhare@gmail.com>
2026-06-06 23:40:14 +0530

    22

--- daily.py
+++ daily.py
@@ -319,3 +319,12 @@
 #         percentage = (count * 100 ) // len(s) 
         
 #         return int(percentage)
+
+# 24 Jewels and Stones (leetcode 771)
+# class Solution:
+#     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+#         count = 0
+#         for stone in stones:
+#             if stone in jewels:
+#                 count += 1
+#         return count
