06c2eaa9e656f205773335a83d92ec2f92c6afd6
Anshman Khare <anshmankhare@gmail.com>
2026-07-01 23:40:13 +0530

    32

--- daily.py
+++ daily.py
@@ -422,3 +422,12 @@
 #             return [seen[complement], i]
 
 #         seen[nums[i]] = i
+
+#32 Josephus problem
+# class Solution:
+#     def josephus(self, n, k):
+#         # code here
+#         res=0
+#         for i in range (2,n+1):
+#             res = (res+k) % i;
+#         return res+1
